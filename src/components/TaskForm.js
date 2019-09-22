521dab502d3378e708f62e21c996950851f63375
unknown <guilhermekuni@gmail.com>
2019-09-22 11:42:54 -0300

    Implements save button and save validations

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,24 +1,35 @@
 import React, { useState, useContext } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import { Paper, TextField, MenuItem } from '@material-ui/core';
+import { Paper, TextField, MenuItem, Button, Box } from '@material-ui/core';
+import SendIcon from '@material-ui/icons/Send';
 
 import { priorityLevels } from '../utils/constants';
 import { TaskContext } from '../screens/Home';
 
 const useStyles = makeStyles(theme => ({
   container: {
+    marginTop: theme.spacing(2),
     display: 'flex',
     flexWrap: 'wrap',
   },
   textField: {
-    marginTop: theme.spacing(4),
+    marginTop: theme.spacing(2),
     marginBottom: theme.spacing(4),
-    marginLeft: theme.spacing(4)
+    marginRight: theme.spacing(4),
   },
   selectField: {
     width: 200,
-  }
+  },
+  button: {
+    maxHeight: 54,
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+    marginRight: theme.spacing(4),
+  },
+  sendIcon: {
+    scrollMarginLeft: theme.spacing(1),
+  },
 }));
 
 export default function TaskForm() {
@@ -39,6 +50,8 @@ export default function TaskForm() {
   function handleSubmit(event) {
     event.preventDefault();
 
+    if (taskTitle.length === 0) return;
+
     const lastId = tasks.length > 0 ? tasks[tasks.length - 1].id : 0;
 
     const newTask = {
@@ -54,33 +67,44 @@ export default function TaskForm() {
   return (
     <>
       <Paper>
-        <form className={classes.container} onSubmit={handleSubmit}>
-          <TextField
-            id='taskTitle'
-            label='Title'
-            className={classes.textField}
-            value={taskTitle}
-            onChange={handleDescriptionChange}
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            id="taskPriority"
-            select
-            label="Priority"
-            className={`${classes.textField} ${classes.selectField}`}
-            value={taskPriority.value}
-            onChange={handlePriorityChange}
-            margin="normal"
-            variant="outlined"
-          >
-            {priorityLevels.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.name}
-              </MenuItem >
-            ))}
-          </TextField>
-        </form>
+        <Box display="flex" justifyContent="center">
+          <form className={classes.container} onSubmit={handleSubmit}>
+            <TextField
+              id='taskTitle'
+              label='Title'
+              className={classes.textField}
+              value={taskTitle}
+              onChange={handleDescriptionChange}
+              margin="normal"
+              variant="outlined"
+            />
+            <TextField
+              id="taskPriority"
+              select
+              label="Priority"
+              className={`${classes.textField} ${classes.selectField}`}
+              value={taskPriority.value}
+              onChange={handlePriorityChange}
+              margin="normal"
+              variant="outlined"
+            >
+              {priorityLevels.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem >
+              ))}
+            </TextField>
+            <Button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              className={classes.button}
+              disabled={taskTitle.length === 0}>
+              Save Task
+            <SendIcon className={classes.sendIcon} />
+            </Button>
+          </form>
+        </Box>
       </Paper>
     </>
   );
